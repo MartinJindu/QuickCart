3b54a641840dea22f241a802d75d708d2aec08c1
Jindu <Jindu@gmail.com>
2025-04-01 22:24:26 +0100

    bug fix

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -46,7 +46,46 @@ const OrderSummary = () => {
     setIsDropdownOpen(false);
   };
 
-  const createOrder = async () => {};
+  const createOrder = async () => {
+    try {
+      if (!selectedAddress) {
+        return toast.error("Please select an address");
+      }
+
+      let cartItemArray = Object.keys(cartItems).map((key) => ({
+        product: key,
+        quantity: cartItems[key],
+      }));
+
+      // removing items that have been removed by users but still in DB
+      cartItemArray = cartItemArray.filter((item) => item.quantity > 0);
+
+      if (cartItemArray.length === 0) {
+        return toast.error("Cart is empty");
+      }
+
+      const token = await getToken();
+      const { data } = await axios.post(
+        "/api/order/create",
+        {
+          address: selectedAddress._id,
+          items: cartItemArray,
+        },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+
+        setCartItems([]);
+        router.push("/order-placed");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   useEffect(() => {
     if (user) {
