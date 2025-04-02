43417aa52b1775621d194090dfbdefb241ac7c6a
Jindu <Jindu@gmail.com>
2025-04-02 21:35:42 +0100

    third commit and fix product display  on homepage

---
+++ app/api/order/list/route.js
@@ -0,0 +1,26 @@
+import connectDB from "@/config/db";
+import Address from "@/models/address";
+import Order from "@/models/order";
+import Product from "@/models/product";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// to get list of orders for user
+export async function GET(req) {
+  try {
+    const { userId } = getAuth(req);
+
+    await connectDB();
+
+    Address.length;
+    Product.length;
+
+    const orders = await Order.find({ userId }).populate(
+      "address items.product"
+    );
+
+    return NextResponse.json({ success: true, orders });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
+}
